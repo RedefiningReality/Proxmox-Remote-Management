035acbe953e297d7013b4cc73739f68fb69f5005
John Ford <johnthejolly@gmail.com>
2023-04-27 15:38:12 -0500

    Update easypurge.sh

--- easypurge.sh
+++ easypurge.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Runs purge with a set of predefined arguments
+# $1 -> 1st command line argument
+# $2 -> 2nd command line argument
+# ...
+# ex. purge doge => remove user doge and associated virtual machines and bridges
 purge $1 -u -b -bv 402 -v
